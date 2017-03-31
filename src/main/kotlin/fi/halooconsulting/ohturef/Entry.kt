c918ed0f7f23746174c30fd86667604d88a9c1ba
Paavo Huhtala <paavo.huhtala@gmail.com>
2017-03-31 13:55:07 +0300

    Päivitä Kotlin, lisää Handlebars

--- src/main/kotlin/fi/halooconsulting/ohturef/Entry.kt
+++ src/main/kotlin/fi/halooconsulting/ohturef/Entry.kt
@@ -4,7 +4,6 @@ package fi.halooconsulting.ohturef
 import fi.halooconsulting.ohturef.web.Server
 
 fun main(args: Array<String>) {
-    println("Hello, Ohtu!")
     val server = Server()
     server.start()
 }
--- src/main/kotlin/fi/halooconsulting/ohturef/web/Server.kt
+++ src/main/kotlin/fi/halooconsulting/ohturef/web/Server.kt
@@ -1,15 +1,24 @@
 package fi.halooconsulting.ohturef.web
 
 import spark.Spark.*
+import spark.ModelAndView
+import spark.template.handlebars.HandlebarsTemplateEngine
 
-class Server() {
+class Server {
     init {
         println("Created Ohturef server.")
     }
 
     fun start() {
+        val templateEngine = HandlebarsTemplateEngine()
+
         port(8000)
-        get("/:id", { req, res -> "Hello, ${req.params("id")}!"})
+
+        get("/:id", { req, res ->
+            val vars = hashMapOf("name" to req.params("id"))
+            ModelAndView(vars, "index.hbs")
+        }, templateEngine)
+
         println("Started Ohturef server in port ${port()}")
     }
 }
