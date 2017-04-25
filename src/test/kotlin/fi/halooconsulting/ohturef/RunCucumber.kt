0e64acc5550beddbc0ba28c6df7b93281154166a
Sara Tolvanen <saratolv@dhcp-asv-121.helsinki.fi>
2017-04-25 22:25:01 +0300

    tagin lisäyskenttä reference sivulle

--- src/test/kotlin/fi/halooconsulting/ohturef/RunCucumber.kt
+++ src/test/kotlin/fi/halooconsulting/ohturef/RunCucumber.kt
@@ -1,9 +1,31 @@
 package fi.halooconsulting.ohturef
 
+import fi.halooconsulting.ohturef.web.Server
 import cucumber.api.CucumberOptions
 import cucumber.api.junit.Cucumber
+import fi.halooconsulting.ohturef.database.Database
+import io.requery.sql.TableCreationMode
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(plugin = arrayOf("pretty"))
-class RunCucumberTests
+class RunCucumberTests {
+    companion object {
+
+        val db = Database.sqlite(creationMode = TableCreationMode.DROP_CREATE)
+        val server = Server(db)
+
+        @BeforeClass @JvmStatic
+        fun setup() {
+            server.start()
+        }
+
+        @AfterClass @JvmStatic
+        fun teardown() {
+            server.stop()
+        }
+
+    }
+}
