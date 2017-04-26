c90d848773c2571e6cedca36a2120412a67a3a05
Paavo Huhtala <paavo.huhtala@gmail.com>
2017-04-26 13:20:37 +0300

    Lisää id-generaattori käyttöliittymään

--- public/js/new.js
+++ public/js/new.js
@@ -5,12 +5,28 @@ function hideFields() {
 }
 
 $(document).ready(function() {
+    $('#submit').hide();
     $('#type').change(function() {
         hideFields();
+        $('#submit').show();
         var enabledFields = $('#type').find('option:selected').data('show').split(" ");
         enabledFields.forEach(function(f) {
             $('.' + f).show();
             $('#' + f).prop('disabled', false);
+        });
+    });
+
+    $('#generate-id').click(function () {
+        $.ajax({
+            url: "/generate_id",
+            method: "POST",
+            dataType: "text",
+            contentType: "application/json",
+            data: JSON.stringify({
+                year: $('#year').val(),
+                authors: [$('#author').val()]
+            }),
+            success: function(newId) { $('#id').val(newId) }
         });
     });
 
