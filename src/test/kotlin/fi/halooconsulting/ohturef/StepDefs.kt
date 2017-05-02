7aa8f1c1a1671b6b36502f07d1318b8a609c51c3
Julius Laitala <jiilaitala@gmail.com>
2017-05-02 21:18:22 +0300

    Changed saucelab port, added build number

--- src/test/kotlin/fi/halooconsulting/ohturef/StepDefs.kt
+++ src/test/kotlin/fi/halooconsulting/ohturef/StepDefs.kt
@@ -27,7 +27,8 @@ class StepDefs {
         caps.setCapability("platform", "Linux")
         caps.setCapability("version", "45.0")
         caps.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"))
-        driver = RemoteWebDriver(URL("http://$username:$accessKey@ondemand.saucelabs.com:443/wd/hub"), caps)
+        caps.setCapability("build", System.getenv("TRAVIS_BUILD_NUMBER"))
+        driver = RemoteWebDriver(URL("http://$username:$accessKey@ondemand.saucelabs.com:4445/wd/hub"), caps)
     }
 
     @Given("^main page is loaded$")
